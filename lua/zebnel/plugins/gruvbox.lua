b1b6d496f1af7b2b0535c9c526028cef82e6c56b
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2025-10-16 11:26:41 -0400

    Change: disable italics for gruvbox theme

--- lua/zebnel/plugins/gruvbox.lua
+++ lua/zebnel/plugins/gruvbox.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_enable_italic = false
     vim.g.gruvbox_material_background = 'hard'
     vim.cmd.colorscheme('gruvbox-material')
   end
